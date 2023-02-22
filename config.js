028935e8751d3f6dde701885d57cffdb500f3d44
blackdragonbot <123376007+blackdragonbot@users.noreply.github.com>
2023-02-22 15:24:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,9 +21,9 @@ global.APIKeys = {
 }
 ////////////////////////////////////////////////////|:|             OWNER DETAILS             |:|//////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-global.session = process.env.SESSION_ID || 'PUT HERE',
-global.ownername = process.env.OWNER_NAME === undefined ? "DarkWinzo" : process.env.OWNER_NAME
-let fake = process.env.OWNER_NUMBER === undefined ? '94775200935' : process.env.OWNER_NUMBER
+global.session = process.env.SESSION_ID || 'VUpDcExiRDY=',
+global.ownername = process.env.OWNER_NAME === undefined ? "𝐑𝐄𝐀⃞𝐋 ₦Ø₮Ɏ🌹⃧⃞ 𝙾𝙵⃞𝙲" : process.env.OWNER_NAME
+let fake = process.env.OWNER_NUMBER === undefined ? '94716829998' : process.env.OWNER_NUMBER
 global.owner = [`${fake}`]
 global.ownernumber = [`${fake}`]
 global.premium = [`${fake}`]
